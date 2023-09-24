fea4b96df185867345a96021ff3c783308e12b67
JianWXu <jianwxu88@gmail.com>
2023-09-24 01:46:46 -0700

    constructor class made
    all the methods for MarkovMachine done and makeText.js initialized

--- markov.js
+++ markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -17,13 +15,39 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let obj = new Map();
+    for (let i = 0; i < this.words.length; i++) {
+      if (obj.has(this.words[i])) {
+        obj.get(this.words[i]).push(this.words[i + 1] || null);
+      } else {
+        obj.set(this.words[i], [this.words[i + 1]] || null);
+      }
+    }
+    this.chains = obj;
   }
 
+  static choice(ar) {
+    return ar[Math.floor(Math.random() * ar.length)];
+  }
 
   /** return random text from chains */
 
-  makeText(numWords = 100) {
-    // TODO
+  makeText(numWords = 8) {
+    let allKeys = Array.from(this.chains.keys());
+    let randKey = MarkovMachine.choice(allKeys);
+    let arr = [];
+
+    while (randKey !== null && arr.length < numWords) {
+      arr.push(randKey);
+      randKey = MarkovMachine.choice(this.chains.get(randKey));
+    }
+
+    return arr.join(" ");
   }
+  
 }
+
+
+module.exports = {
+  MarkovMachine,
+};
